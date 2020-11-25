b5463f280481f0bf9bc68af03751c72c0fec80c1
Thomas Kirst <thomas@moneygarden.de>
2020-11-25 16:09:07 +0100

    Removed storybook

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,8 @@
 import React from "react";
 import "./App.css";
-import Page from "./pages/Page";
 
 function App() {
-  return (
-    <div className="App">
-      <Page
-        onLogin={() => console.log("login")}
-        onLogout={() => console.log("logout")}
-        onCreateAccount={() => console.log("create account")}
-      />
-      <a href="/storybook">To Storybook</a>
-    </div>
-  );
+  return <div className="App">Hello</div>;
 }
 
 export default App;
