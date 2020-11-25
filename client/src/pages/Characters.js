0cc11856f2c559b4722b8c511b9833c269ee87ce
Thomas Kirst <thomas@moneygarden.de>
2020-11-25 16:52:43 +0100

    Made the server understand ES modules

---
+++ client/src/pages/Characters.js
@@ -0,0 +1,7 @@
+import { getLineAndCharacterOfPosition } from 'typescript';
+
+import React from 'react';
+
+export default () => {
+  <h1>Characters</h1>;
+};
--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
-const express = require("express");
-const path = require("path");
+import express from 'express';
+import path from 'path';
+import { dirname } from './helpers.js';
+
+const __dirname = dirname(import.meta.url);
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 // Serve any static files
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Handle React routing, return all requests to React app
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(port, () => {
