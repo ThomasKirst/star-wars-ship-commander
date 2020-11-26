baf0b3822adffc646c8864a6788e844da83181ab
Thomas Kirst <thomas.kirst@neuefische.de>
2020-11-26 15:04:30 +0100

    Fixed prettier issues due to new configuration

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-test("renders storybook react link", () => {
+test('renders storybook react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/To Storybook/i);
   expect(linkElement).toBeInTheDocument();
